76783c229b0eae38e72806139ee374241f853ce1
socHACKi <johnsochacki@hotmail.com>
2020-11-27 17:01:33 -0500

    Update install_and_setup_vimtex_ubuntu20p04.sh

--- install_and_setup_vimtex_ubuntu20p04.sh
+++ install_and_setup_vimtex_ubuntu20p04.sh
@@ -67,6 +67,13 @@ pip3.8 install --user .
 cd ../
 rm -rf inkscape-latex-figures
 
+# Install the python version i made for diagrams.net images
+git clone https://github.com/jsochacki/diagrams-net-figures.git
+cd diagrams-net-figures
+pip3.8 install --user .
+cd ../
+rm -rf diagrams-net-figures
+
 #inkscape shortcut manager setup
 #cd $TMPOUT/.local/lib/python3.8/site-packages/
 #git clone https://github.com/python-xlib/python-xlib.git
@@ -474,8 +481,11 @@ autocmd BufNewFile * :write
 autocmd CursorHold,CursorHoldI * update
 set updatetime=500
 
-inoremap <C-f> <Esc>: silent exec '.!inkscape-figures create "'.getline('.').'" "'.b:vimtex.root.'/figures/"'<CR><CR>:w<CR>
-nnoremap <C-f> : silent exec '!inkscape-figures edit "'.b:vimtex.root.'/figures/" > /dev/null 2>&1 &'<CR><CR>:redraw!<CR>
+inoremap <F1> <Esc>: silent exec '.!inkscape-figures create "'.getline('.').'" "'.b:vimtex.root.'/figures/"'<CR><CR>:w<CR>
+nnoremap <F1> : silent exec '!inkscape-figures edit "'.b:vimtex.root.'/figures/" > /dev/null 2>&1 &'<CR><CR>:redraw!<CR>
+
+inoremap <F2> <Esc>: silent exec '.!diagrams-net-figures latex-create "'.getline('.').'" "'.b:vimtex.root.'/figures/"'<CR><CR>:w<CR>
+nnoremap <F2> : silent exec '!diagrams-net-figures edit "'.b:vimtex.root.'/figures/" > /dev/null 2>&1 &'<CR><CR>:redraw!<CR>
 
 "This is required to get actual auto figure insertion
 "along with the installation of the update_tex_figures.sh script
@@ -498,6 +508,65 @@ function ShutdownFunctions()
   call vimtex#compiler#clean(0)
   silent exec "!kill $(ps aux | grep '[p]ython3.*main.py' | awk '{print $2}')"
 endfunction
+
+" Activate this with K (shift-k)
+"command -nargs=1 Googleit :!python3 ~/.vimfiles/Googleit.py <args>
+"set keywordprg=:Googleit
+
+EOF
+
+
+cat >> ~/.vim/ftplugin/markdown.vim << 'EOF'
+" markdown specific settings
+
+" Crucial to allow ycm and ultisnips to work together
+" cant tab complete ultisnips if you don't do this
+" free per https://vim.fandom.com/wiki/Unused_keys
+let g:ycm_key_list_select_completion=['<C-B>']
+let g:ycm_key_list_previous_completion=['<C-J>']
+
+" Load YouCompleteMe
+packadd YouCompleteMe
+
+" Saves as soon as you make the file so compilation works
+autocmd BufNewFile * :write
+
+" I turn on autosave on no activity in 500ms here for insert and non insert mode
+" many may not like this but after turning on continuous compilation with \ll
+" you get live updates without haveing to save manually in tex so im just putting this
+" in the tex specific file configuration
+autocmd CursorHold,CursorHoldI * update
+set updatetime=500
+
+"TODO update the inkscape-figures repo to accomodate inkscape files in markdown
+"inoremap <F1> <Esc>: silent exec '.!inkscape-figures create "'.getline('.').'" "'.b:vimtex.root.'/figures/"'<CR><CR>:w<CR>
+"nnoremap <F1> : silent exec '!inkscape-figures edit "'.b:vimtex.root.'/figures/" > /dev/null 2>&1 &'<CR><CR>:redraw!<CR>
+
+inoremap <F2> <Esc>: silent exec '.!diagrams-net-figures markdown-create "./figures/"'<CR><CR>:w<CR>
+nnoremap <F2> : silent exec '!diagrams-net-figures edit "./figures/" > /dev/null 2>&1 &'<CR><CR>:redraw!<CR>
+
+"This is required to get actual auto figure insertion
+"along with the installation of the update_tex_figures.sh script
+autocmd CursorHold * : silent exec '!update_tex_figures.sh ' expand('%:p:h')
+
+
+" Starts autocompilation at .tex file open and cleansup at close and shortcut watcher
+augroup vimtex_event_1
+  au!
+  au User VimtexEventQuit     call ShutdownFunctions()
+  au User VimtexEventInitPost call StartupFunctions()
+augroup END
+
+" TODO need to modify for markdown rendered with pandoc and opened with zathura
+" Autorun and kill shortcut watcher run at .tex file launch each time and turn off when done
+"function StartupFunctions()
+  "call vimtex#compiler#compile()
+  "silent exec '!python3 ~/.local/lib/python3.8/site-packages/inkscape-latex-shortcuts/main.py &'
+"endfunction
+"function ShutdownFunctions()
+  "call vimtex#compiler#clean(0)
+  "silent exec "!kill $(ps aux | grep '[p]ython3.*main.py' | awk '{print $2}')"
+"endfunction
 
 " Activate this with K (shift-k)
 "command -nargs=1 Googleit :!python3 ~/.vimfiles/Googleit.py <args>
@@ -571,5 +640,8 @@ vim +PlugInstall +qa
 
 # Install a decent browser
 ./Chrome_Setup.sh
+
+# install diagrams.net
+sudo snap install drawio
 
 source ~/.bashrc
