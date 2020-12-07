9b64302b110896390c98b90426c9800c5c80fc73
socHACKi <johnsochacki@hotmail.com>
2020-12-07 11:16:24 -0500

    Update setup_latex_dir.sh

--- setup_latex_dir.sh
+++ setup_latex_dir.sh
@@ -4,5 +4,6 @@ FULL_PATH_WITH_DIR=$1
 CURDIR=$(pwd)
 
 mkdir -p $FULL_PATH_WITH_DIR/figures
+mkdir -p $FULL_PATH_WITH_DIR/images
 cp $CURDIR/None.rasi $FULL_PATH_WITH_DIR/
 cp $CURDIR/systems_book.sty $FULL_PATH_WITH_DIR/
