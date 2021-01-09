5d96f808fa697d14ede2217287833612922e8fee
jsochacki <johnsochacki@hotmail.com>
2021-01-09 04:36:43 +0000

    adding gitkeeps

--- setup_latex_dir.sh
+++ setup_latex_dir.sh
@@ -8,6 +8,11 @@ mkdir -p $FULL_PATH_WITH_DIR/images
 mkdir -p $FULL_PATH_WITH_DIR/csv-data
 mkdir -p $FULL_PATH_WITH_DIR/gp-files
 mkdir -p $FULL_PATH_WITH_DIR/build
+touch $FULL_PATH_WITH_DIR/figures/.gitkeep
+touch $FULL_PATH_WITH_DIR/images/.gitkeep
+touch $FULL_PATH_WITH_DIR/csv-data/.gitkeep
+touch $FULL_PATH_WITH_DIR/gp-files/.gitkeep
+touch $FULL_PATH_WITH_DIR/build/.gitkeep
 cp $CURDIR/None.rasi $FULL_PATH_WITH_DIR/
 cp $CURDIR/systems_book.sty $FULL_PATH_WITH_DIR/
 cp $CURDIR/systems_book_md.sty $FULL_PATH_WITH_DIR/
