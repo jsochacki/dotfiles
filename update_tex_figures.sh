dbbdf2f97d8d5351e9a093c728e19bed583dfba1
jsochacki <johnsochacki@hotmail.com>
2021-01-14 05:07:51 +0000

    added regex to keep latex compiled firgures from my scripts from adding duplicate pages for due to the output of inkscapes pdf_tex conversion

--- update_tex_figures.sh
+++ update_tex_figures.sh
@@ -88,6 +88,13 @@ do
     sed -i 's@<switch><g requiredFeatures="http://www.w3.org/TR/SVG11/feature#Extensibility"/><a transform="translate(0,-5)" xlink:href="https://www.diagrams.net/doc/faq/svg-export-text-problems" target="_blank"><text text-anchor="middle" font-size="10px" x="50%" y="100%">Viewer does not support full SVG 1.1</text></a></switch>@@' $i
     # export the pdf and pdf_tex files
     inkscape $i --export-area-page --export-dpi 300 --export-pdf ${i%.*}.pdf --export-latex ${i%.*}.svg
+
+    #inkscape has issues generating pdf_tex and makes multiple pdf includes
+    #accidentally so we will remove them as they make files huge
+    # Remove anythings that isn't page=1]
+    sed -ri '/\\put\(0,0\)\{\\includegraphics\[width\=\\unitlength,page=1](.*)/!s/\\put\(0,0\)\{\\includegraphics\[width\=\\unitlength,page=[[:digit:]]+](.*)//g' ${i%.*}.pdf_tex
+    #remove spaces left behind
+    sed -ri '/^[[:space:]]*$/d' ${i%.*}.pdf_tex
   fi
 done
 
