60ec766460c7d40d3b480471cfba687a55e674d1
jsochacki <johnsochacki@hotmail.com>
2021-11-08 19:59:31 +0000

    update to docxtomd

--- docxtomarkdown.sh
+++ docxtomarkdown.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+mkdir tempworkingdir
+mv "$1".docx tempworkingdir
+cd tempworkingdir
 pandoc -s -f docx -t markdown --extract-media ./ "$1".docx -o "$1".md
+mv "$1".md ../
 cd media
 #libreoffice --headless --convert-to png *.emf
 libreoffice --headless --convert-to pdf *.*
@@ -12,7 +16,7 @@ for file in image*.png
 do
    mv "$file" "${file/image/"$1"image}"
 done
-mv *.png ../images
-cd ../
-rm -rf media
+mv *.png ../../images
+cd ../../
+rm -rf tempworkingdir
 
