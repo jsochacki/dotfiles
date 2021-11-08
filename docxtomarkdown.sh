a2a46e27231314334538822ba0e3a771989d0446
jsochacki <johnsochacki@hotmail.com>
2021-11-08 19:28:20 +0000

    updating docxtomd for high quality images

--- docxtomarkdown.sh
+++ docxtomarkdown.sh
@@ -2,13 +2,17 @@
 
 pandoc -s -f docx -t markdown --extract-media ./ "$1".docx -o "$1".md
 cd media
-libreoffice --headless --convert-to png *.emf
-rm *.emf
+#libreoffice --headless --convert-to png *.emf
+libreoffice --headless --convert-to pdf *.*
+for file in image*.pdf
+do
+   convert -density 600 -trim "$file" "${file/.pdf/.png}"
+done
 for file in image*.png
 do
    mv "$file" "${file/image/"$1"image}"
 done
-mv * ../images
+mv *.png ../images
 cd ../
 rm -rf media
 
